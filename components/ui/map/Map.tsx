bbec741b62969274b6b4d0d116e6aeeb7fedfebe
Adilxa <adilhanimerov82@gmail.com>
2024-11-17 01:57:53 +0600

    fix:map

--- components/ui/map/Map.tsx
+++ components/ui/map/Map.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import MAPIMG from "../../../assets/png/map.png";
 import Image from "next/image";
 import "./styles.scss";
-import useWindowDimensions from "../../../hooks/useDimensions"
+import useWindowDimensions from "../../../hooks/useDimensions";
 
 function Map() {
-
-    const { height } = useWindowDimensions();
+    const { height, width } = useWindowDimensions(); // Get both width and height
 
     return (
         <div className="map-container"
             style={{
-                height: `${height}px`
+                height: `${height}px`,  // Set height dynamically based on viewport
             }}
         >
             <Image
